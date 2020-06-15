6b09c234a3c6f6601e05f981d83c2d91660dcd44
Diarcip <diegocifuentes98@hotmail.com>
2020-06-15 18:28:29 -0500

    Add new textbox

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function suma(){
   var val1=parseInt(document.getElementById("N1").value);
   var val2=parseInt(document.getElementById("N2").value);
-  alert(val1+val2);
+  var val3=parseInt(document.getElementById("N3").value);
+  alert(val1+val2+val3);
 }
