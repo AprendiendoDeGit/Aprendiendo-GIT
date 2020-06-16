e60e724a029b5e4176c4c6d90e7ba1e49c5fe487
Diarcip <diegocifuentes98@hotmail.com>
2020-06-15 19:00:50 -0500

    Se agrega el cuarto checkbox

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ function suma(){
   var val1=parseInt(document.getElementById("N1").value);
   var val2=parseInt(document.getElementById("N2").value);
   var val3=parseInt(document.getElementById("N3").value);
-  alert(val1+val2+val3);
+  var val4=parseInt(document.getElementById("N4").value);
+  alert(val1+val2+val3+val4);
 }
